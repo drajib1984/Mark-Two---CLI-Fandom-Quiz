ab160a2385331ea94a459398a572ec19947f61e7
drajib1984 <106616936+drajib1984@users.noreply.github.com>
2023-01-14 22:37:21 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,67 +1,78 @@
 // Make a game => ask questions, make a function and arraylist of questions
 // Diplay the highest score & player score
 
-var readlineSync = require("readline-sync");
-
-var userName = readlineSync.question("What is your name? ");
-console.log("Hello "+userName+", Welcome to the QUIZ - DO YOU KNOW RAJIB!!");
-
+var readLineSynce = require("readline-sync");
 var score = 0;
 
-function checkAnswers(question,answer){
-  var userAnswer = readlineSync.question(question);
-
-  if(userAnswer==answer){
-    console.log("You are right!!");
-    score+=2;
-  }
-  else{
-    console.log("you are wrong!!");
-    score--;
-  }
+function welcomeUser(){
+var userName = readLineSynce.question("Enter your name: ");
+console.log("Welcome "+userName+", Lets play a quiz on Indian History. Enter the option that you think correct");
 }
 
 var questionList = [
   {
-    question : "What is Rajib's graduation year?",
-    answer : 2007
+    question : "What is the number of states in India?\na. 14\nb. 16\nc. 25\nd. 28 ",
+    answer : "d"
   },
   {
-    question : "Which College did Rajib study?",
-    answer : "NIT,Agartala"
+    question : "When was Burma was separated from India?\na. 1948\nb. 1901\nc. 1937\nd. 1945 ",
+    answer : "c"
   },
   {
-    question : "Where is Rajib's hometown?",
-    answer : "Dharmanagar"
+    question : "When did India become a republic?\na. 1950\nb. 1947\nc. 1949\nd. 1961 ",
+    answer : "a"
   },
   {
-    question : "Which year Rajib was born?",
-    answer : 1984
+    question : "Which state was known as North East Frontier Agency?\na. Mizoram\nb. Arunachal Pradesh\nc. Manipur\nd. Tripura ",
+    answer : "b"
   },
   {
-    question : "Which school did Rajib go?",
-    answer : "Golden Valley School"
-  } 
-]
-for(var i =0; i<questionList.length; i++){
-  var currentQuestion=questionList[i];
-  checkAnswers(currentQuestion.question,currentQuestion.answer);
-}
-console.log("Your score is: "+score);
-var highestScores=[
-  {
-    name : "Ranjit",
-    score : 10
-  },{
-    name : "Ananya",
-    score : 9
+   question : "Which state or union territory has French as an official language?\na. Goa\nb. Lakshadweep\nc. Pondicherry\nd. Diu and Daman ",
+    answer : "c"
   }
 ]
 
+function checkAnswer(question, answer){
+  var userAnswer = readLineSynce.question(question);
 
-for(var i =0; i<highestScores.length; i++){
-  var currentScorer=highestScores[i];
-  console.log("The highest scorers are: "+currentScorer.name+" & score is: "+currentScorer.score);
+  if(userAnswer==answer){
+    console.log("Hooray !! You are right");
+    score +=2; 
+  }
+  else{
+    console.log ("Sorry !! It's a wrong answer");
+    console.log("Correct answer is :"+answer);
+    score--;
+  }
 }
+
+function playQuiz(){
+for(var i =0; i<questionList.length; i++){
+  var currentQuestion = questionList[i];
+  checkAnswer(currentQuestion.question,currentQuestion.answer);
+}
+}
+var winnersList = [
+   {
+     name  : "Ranjit", score : 10
+   },
+   {
+     name : "Ira", score : 9
+   }
+]
+
+function displayScore(){
+  console.log ("Thank you for taking part in the quiz. Your final score is : "+score);
+  for(var i =0; i<winnersList.length; i++){
+    var currentWinner = winnersList[i];
+    console.log("The highest scorer is: "+currentWinner.name+" & score is: "+currentWinner.score);
+  }
+  console.log("If you meet the highest score, send me a screenshot");
+}
+
+welcomeUser();
+playQuiz();
+displayScore();
+
 
 
